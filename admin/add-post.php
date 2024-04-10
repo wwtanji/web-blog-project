3824ca20e302a7be46faff81d9708efc6cd979d0
Roman Zotsenko <romazotsenko15@gmail.com>
2024-04-10 12:02:56 +0200

    Admin panel, crud operacii, search

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,154 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Multipage Web-Site</title>
-    <!-- Custom Stylesheet -->
-    <link rel="stylesheet" href="./style.css">
-    <!-- ICONSCOUT CDN -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
-    <!-- Google fonts MONTSERRAT-->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<?php
+include 'partials/header.php';
 
-</head>
-<body>
+// fetch categories from database
+$query = "SELECT * FROM categories";
+$categories = mysqli_query($connection, $query);
 
-    <nav>
-        <div class="container nav__container">
-            <a href="index.html" class="nav__logo">Romii</a>
-            <ul class="nav__items">
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="contact.html">Contact</a></li>
-        
-                <li class="nav__profile">
-                    <div class="avatar">
-                        <img src="./images/avatar1.jpg" alt="User Avatar">
-                    </div>
-                    <ul>
-                        <li><a href="dashboard.html">Dashboard</a></li>
-                        <li><a href="logout.html">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <button id="open__nav-btn"><i class="uil uil-bars"></i></button>
-            <button id="close__nav-btn"><i class="uil uil-multiply"></i></button>
-        </div>
-    </nav>
-    <!-- END OF NAV -->
+// get back form data if form was invalid
+$title = $_SESSION['add-post-data']['title'] ?? null;
+$body = $_SESSION['add-post-data']['body'] ?? null;
 
-
+// delete form data session
+unset($_SESSION['add-post-data']);
+?>
 
 
 
 <section class="form__section">
     <div class="container form__section-container">
         <h2>Add Post</h2>
-        <div class="alert__message error">
-            <p>This is an error message</p>
-        </div>
-        <form action="" enctype="multipart/form-data">
-            <input type="text" placeholder="Title">
-            <select>
-                <option value="1">Travel</option>
-                <option value="1">Art</option>
-                <option value="1">Science and Techonogy</option>
-                <option value="1">Travel</option>
-                <option value="1">Travel</option>
-                <option value="1">Travel</option>
-                <option value="1">Travel</option>
-                <option value="1">Travel</option>
+        <?php if (isset($_SESSION['add-post'])) : ?>
+            <div class="alert__message error">
+                <p>
+                    <?= $_SESSION['add-post'];
+                    unset($_SESSION['add-post']);
+                    ?>
+                </p>
+            </div>
+        <?php endif ?>
+        <form action="<?= ROOT_URL ?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
+            <input type="text" name="title" value="<?= $title ?>" placeholder="Title">
+            <select name="category">
+                <?php while ($category = mysqli_fetch_assoc($categories)) : ?>
+                    <option value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
+                <?php endwhile ?>
             </select>
-            <textarea rows="10" placeholder="Body"></textarea>
-            <div class="form__control inline">
-                <input type="checkbox" id="is__featured"checked>
-                <label for="is__featured">Featured</label>
-            </div>
+            <textarea rows="10" name="body" placeholder="Body"><?= $body ?></textarea>
+            <?php if (isset($_SESSION['user_is_admin'])) : ?>
+                <div class="form__control inline">
+                    <input type="checkbox" name="is_featured" value="1" id="is_featured" checked>
+                    <label for="is_featured">Featured</label>
+                </div>
+            <?php endif ?>
             <div class="form__control">
-                <label for="thumbail">Add Thumbail</label>
-                <input type="file" id="thumbail">
+                <label for="thumbnail">Add Thumbnail</label>
+                <input type="file" name="thumbnail" id="thumbnail">
             </div>
-            <button type="submit" class="btn">Add Post</button>
+            <button type="submit" name="submit" class="btn">Add Post</button>
         </form>
     </div>
 </section>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<footer>
-    <div class="footer__socials">
-        <a href="https://youtube.com" target="_blank"><i class="uil uil-youtube"></i></a>
-        <a href="https://facebook.com" target="_blank"><i class="uil uil-facebook-f"></i></a>
-        <a href="https://instagram.com" target="_blank"><i class="uil uil-instagram-alt"></i></a>
-        <a href="https://twitter.com" target="_blank"><i class="uil uil-twitter"></i></a>
-    </div>
-    <div class="container footer__container">
-        <article>
-            <h4>Categories</h4>
-            <ul>
-                <li><a href="">Art</a></li>
-                <li><a href="">Wild Life</a></li>
-                <li><a href="">Travel</a></li>
-                <li><a href="">Sciecne % Technologys</a></li>
-                <li><a href="">Food</a></li>
-                <li><a href="">Music</a></li>
-            </ul>
-        </article>
-        <article>
-            <h4>Support</h4>
-            <ul>
-                <li><a href="">Online Support</a></li>
-                <li><a href="">Call Numbers</a></li>
-                <li><a href="">Emails</a></li>
-                <li><a href="">Social Support</a></li>
-                <li><a href="">Location</a></li>
-            </ul>
-        </article>
-        <article>
-            <h4>Blog</h4>
-            <ul>
-                <li><a href="">Safety</a></li>
-                <li><a href="">Repair</a></li>
-                <li><a href="">Recent</a></li>
-                <li><a href="">Popular</a></li>
-                <li><a href="">Categories</a></li>
-            </ul>
-        </article>
-        <article>
-            <h4>Permalinks</h4>
-            <ul>
-                <li><a href="">Homer</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">Contact</a></li>
-            </ul>
-        </article>
-    </div>
-    <div class="footer__copyright">
-        <small>Copyright &copy; 2024 Romiii </small>
-    </div>
-</footer>
-
-
-
-
-
-
-</head>
-<body
+<?php
+include '../partials/footer.php';
+?>
--- admin/manage-users.php
+++ admin/manage-users.php
@@ -1,208 +1,122 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Multipage Web-Site</title>
-    <!-- Custom Stylesheet -->
-    <link rel="stylesheet" href="./style.css">
-    <!-- ICONSCOUT CDN -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">    <!-- Google fonts MONTSERRAT-->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<?php
+include 'partials/header.php';
 
-</head>
-<body>
-    
+// fetch users from database but not current user
+$current_admin_id = $_SESSION['user-id'];
 
-
-    
-    <nav>
-        <div class="container nav__container">
-            <a href="index.html" class="nav__logo">Romii</a>
-            <ul class="nav__items">
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="contact.html">Contact</a></li>
-        
-                <li class="nav__profile">
-                    <div class="avatar">
-                        <img src="./images/avatar1.jpg" alt="User Avatar">
-                    </div>
-                    <ul>
-                        <li><a href="dashboard.html">Dashboard</a></li>
-                        <li><a href="logout.html">Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <button id="open__nav-btn"><i class="uil uil-bars"></i></button>
-            <button id="close__nav-btn"><i class="uil uil-multiply"></i></button>
-        </div>
-    </nav>
-    <!-- END OF NAV -->
-
-
-
-
-
-
-
-
-
-
-
+$query = "SELECT * FROM users WHERE NOT id=$current_admin_id";
+$users = mysqli_query($connection, $query);
+?>
 
 
 
 
 <section class="dashboard">
+    <?php if (isset($_SESSION['add-user-success'])) : // shows if add user was successful
+    ?>
+        <div class="alert__message success container">
+            <p>
+                <?= $_SESSION['add-user-success'];
+                unset($_SESSION['add-user-success']);
+                ?>
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['edit-user-success'])) : // shows if edit user was successful
+    ?>
+        <div class="alert__message success container">
+            <p>
+                <?= $_SESSION['edit-user-success'];
+                unset($_SESSION['edit-user-success']);
+                ?>
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['edit-user'])) : // shows if edit user was NOT successful
+    ?>
+        <div class="alert__message error container">
+            <p>
+                <?= $_SESSION['edit-user'];
+                unset($_SESSION['edit-user']);
+                ?>
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['delete-user'])) : // shows if delete user was NOT successful
+    ?>
+        <div class="alert__message error container">
+            <p>
+                <?= $_SESSION['delete-user'];
+                unset($_SESSION['delete-user']);
+                ?>
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['delete-user-success'])) : // shows if delete user was successful
+    ?>
+        <div class="alert__message success container">
+            <p>
+                <?= $_SESSION['delete-user-success'];
+                unset($_SESSION['delete-user-success']);
+                ?>
+            </p>
+        </div>
+    <?php endif ?>
+
     <div class="container dashboard__container">
-        <button id="show__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-right"></i></button>
-        <button id="hide__sidebar-btn"  class="sidebar__toggle"><i class="uil uil-angle-left"></i></button>
+        <button id="show__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-right-b"></i></button>
+        <button id="hide__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-left-b"></i></button>
         <aside>
             <ul>
-                <li><a href="add-post.html"><i class="uil uil-pen"></i>
-                    <h5>Add Post</h5>
-                </a></li>
+                <li>
+                    <a href="add-post.php"><i class="uil uil-pen"></i>
+                        <h5>Add Post</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php"><i class="uil uil-postcard"></i>
+                        <h5>Manage Posts</h5>
+                    </a>
+                </li>
+                <?php if (isset($_SESSION['user_is_admin'])) : ?>
+
+                    <li>
+                        <a href="manage-users.php" class="active"><i class="uil uil-users-alt"></i>
+                            <h5>Manage User</h5>
+                        </a>
+                    </li>
 
 
-                <li><a href="dashboard.html""><i class="uil uil-postcard"></i>
-                    <h5>Manage Posts</h5>
-                </a></li>
-
-
-                <li><a href="add-user.html"><i class="uil uil-user"></i>
-                    <h5>Add User</h5>
-                </a></li>
-
-
-                <li><a href="manage-users.html" class="active"><i class="uil uil-users-alt"></i>
-                    <h5>Manage Users</h5>
-                </a></li>
-
-
-                <li><a href="add-categories.html"><i class="uil uil-edit"></i>
-                    <h5>Add Categories</h5>
-                </a></li>
-                
-
-                <li><a href="manage-categories.html"><i class="uil uil-list-ul"></i>
-                    <h5>Manage categories</h5>
-                </a></li>
+                <?php endif ?>
             </ul>
         </aside>
         <main>
             <h2>Manage Users</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                        <th>Admin</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                    <tr>
-                        <td>Ernest Achiever</td>
-                        <td>achiever</td>
-                        <td><a href="edit-user.html" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.html" class="btn sm danger">Delete</a></td>
-                        <td>Yes</td>
-                   </tr>
-
-                    <tr>
-                        <td>Daniel Vinyo</td>
-                        <td>daniel</td>
-                        <td><a href="edit-category.html" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.html" class="btn sm danger">Delete</a></td>
-
-                        <td>Yes</td>
-                   </tr>
-
-                    <tr>
-                        <td>Sebastiano KYchati</td>
-                        <td>sebko</td>
-                        <td><a href="edit-category.html" class="btn sm">Edit</a></td>
-                        <td><a href="delete-category.html" class="btn sm danger">Delete</a></td>
-
-                        <td>Yes</td>
-                   </tr>
-
-
-
-
-
-                </tbody>
-            </table>
+            <?php if (mysqli_num_rows($users) > 0) : ?>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Delete</th>
+                            <th>Admin</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($user = mysqli_fetch_assoc($users)) : ?>
+                            <tr>
+                                <td><?= "{$user['firstname']} {$user['lastname']}" ?></td>
+                                <td><?= $user['username'] ?></td>
+                                <td><a href="<?= ROOT_URL ?>admin/delete-user.php?id=<?= $user['id'] ?>" class="btn sm danger">Delete</a></td>
+                                <td><?= $user['is_admin'] ? 'Yes' : 'No' ?></td>
+                            </tr>
+                        <?php endwhile ?>
+                    </tbody>
+                </table>
+            <?php else : ?>
+                <div class="alert__message error"><?= "No users found" ?></div>
+            <?php endif ?>
         </main>
     </div>
 </section>
 
 
-
-<footer>
-    <div class="footer__socials">
-        <a href="https://youtube.com" target="_blank"><i class="uil uil-youtube"></i></a>
-        <a href="https://facebook.com" target="_blank"><i class="uil uil-facebook-f"></i></a>
-        <a href="https://instagram.com" target="_blank"><i class="uil uil-instagram-alt"></i></a>
-        <a href="https://twitter.com" target="_blank"><i class="uil uil-twitter"></i></a>
-    </div>
-    <div class="container footer__container">
-        <article>
-            <h4>Categories</h4>
-            <ul>
-                <li><a href="">Art</a></li>
-                <li><a href="">Wild Life</a></li>
-                <li><a href="">Travel</a></li>
-                <li><a href="">Sciecne % Technologys</a></li>
-                <li><a href="">Food</a></li>
-                <li><a href="">Music</a></li>
-            </ul>
-        </article>
-        <article>
-            <h4>Support</h4>
-            <ul>
-                <li><a href="">Online Support</a></li>
-                <li><a href="">Call Numbers</a></li>
-                <li><a href="">Emails</a></li>
-                <li><a href="">Social Support</a></li>
-                <li><a href="">Location</a></li>
-            </ul>
-        </article>
-        <article>
-            <h4>Blog</h4>
-            <ul>
-                <li><a href="">Safety</a></li>
-                <li><a href="">Repair</a></li>
-                <li><a href="">Recent</a></li>
-                <li><a href="">Popular</a></li>
-                <li><a href="">Categories</a></li>
-            </ul>
-        </article>
-        <article>
-            <h4>Permalinks</h4>
-            <ul>
-                <li><a href="">Homer</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Services</a></li>
-                <li><a href="">Contact</a></li>
-            </ul>
-        </article>
-    </div>
-    <div class="footer__copyright">
-        <small>Copyright &copy; 2024 Romiii </small>
-    </div>
-</footer>
-
-
-
-
-<script src="./main.js"></script>
-
-
-</body>
-</html>
+<?php
+include '../partials/footer.php';
+?>
