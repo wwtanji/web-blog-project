a33b50ca0911008e0493ee8ad5825d37cde645bb
Roman Zotsenko <romazotsenko15@gmail.com>
2024-04-10 11:29:13 +0200

    Spojenie z databazou + fix css

--- js/main.js
+++ js/main.js
@@ -21,18 +21,20 @@ closeNavBtn.addEventListener('click', closeNav);
 
 
 
+
 const sidebar = document.querySelector('aside');
 const showSidebarBtn = document.querySelector('#show__sidebar-btn');
-const hideSidebarBtn = document.querySelector('#hide__sidebar-btn'); // Виправлено селектор
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
 
+// shows sidebar on small devices
 const showSidebar = () => {
     sidebar.style.left = '0';
     showSidebarBtn.style.display = 'none';
     hideSidebarBtn.style.display = 'inline-block';
 }
-
+// hides sidebar on small devices
 const hideSidebar = () => {
-    sidebar.style.left = '-100%'; // Приховати бічну панель
+    sidebar.style.left = '-100%';
     showSidebarBtn.style.display = 'inline-block';
     hideSidebarBtn.style.display = 'none';
 }
